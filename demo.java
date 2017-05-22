54564cdeda620ca0a855228e8d304290f71c3fd2
Vidushi <vidushi2207@gmail.com>
2017-05-22 14:04:32 +0530

    adding setDepartment method

--- demo.java
+++ demo.java
@@ -2,4 +2,8 @@ class Demo {
     public static void main(String[] args){
         System.out.println("Hello");
     }
+
+    public void setDepartment(){
+        System.out.println("setDepartment");
+    }
 }
