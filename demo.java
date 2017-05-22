665ef4ef1acb383ee425f4eacdb001bf78ba9e12
Vidushi <vidushi2207@gmail.com>
2017-05-22 13:39:22 +0530

    modifed demo java file.

--- demo.java
+++ demo.java
@@ -2,4 +2,8 @@ class Demo {
     public static void main(String[] args){
         System.out.println("Hello");
     }
+
+    private void setName(){
+        System.out.println("TestName");
+    }
 }
