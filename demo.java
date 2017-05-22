250f641b44c46a13866d208a89a0489f49a3cc00
Vidushi <vidushi2207@gmail.com>
2017-05-22 13:41:49 +0530

    modifed demo java file and added age into it

--- demo.java
+++ demo.java
@@ -2,4 +2,8 @@ class Demo {
     public static void main(String[] args){
         System.out.println("Hello");
     }
+
+    private void setAge(int age){
+        System.out.println("Age");
+    }
 }
