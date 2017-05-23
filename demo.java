ae597c4d0417908922c492b25b616b166b8284dc
Vidushi <vidushi2207@gmail.com>
2017-05-23 11:45:54 +0530

    Commited the demo.java from the refactoring branch with the new method

--- demo.java
+++ demo.java
@@ -6,4 +6,8 @@ class Demo {
     public void setDepartment(){
         System.out.println("setDepartment");
     }
+
+    public void addCommand(){
+        System.out.println("Method Created in refactoring/feature_develop1");
+    }
 }
